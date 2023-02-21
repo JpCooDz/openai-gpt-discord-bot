669a656e1b22bb24896b25ea470585ccbed5c756
João Pedro C Gouveia <52970963+JpCooDz@users.noreply.github.com>
2023-02-21 12:03:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,10 @@ import openai
 
 intents = discord.Intents.all()
 intents.members = True
-bot = commands.Bot(command_prefix='/', intents=intents)
-
- # CHATGPT HELP
-
-load_dotenv()  # carrega as variáveis de ambiente do arquivo .env
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Configuração do OpenAI API
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 model_engine = "text-davinci-002"
 openai_prompt = "Olá, eu sou o ChatGPT. Digite sua mensagem abaixo para começar a conversar comigo!\n\nUsuário:"
 openai_response_start = "\nChatGPT:"
@@ -24,37 +20,28 @@ openai_response_start = "\nChatGPT:"
 # Evento de início do bot
 @bot.event
 async def on_ready():
-    print(f'{bot.user} está conectado ao Discord!')
+    print(f"{bot.user} está conectado ao Discord!")
 
-# Evento de recebimento de mensagem
-@bot.event
-async def on_message(message):
-    # Verifica se a mensagem foi enviada pelo próprio bot
-    if message.author == bot.user:
-        return
+# Comando !chatgpt
+@bot.command(name="chatgpt")
+async def chatgpt(ctx, *, user_message):
+    # Usa o OpenAI API para gerar uma resposta
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=openai_prompt + user_message,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
 
-    # Verifica se a mensagem contém o comando "/chatgpt"
-    if "/chatgpt" in message.content:
-        # Separa a mensagem do usuário do comando "/chatgpt"
-        user_message = message.content.replace("/chatgpt", "").strip()
+    # Extrai a resposta gerada pelo OpenAI API
+    response_text = response.choices[0].text
+    response_text = response_text.replace(openai_response_start, "").strip()
 
-        # Usa o OpenAI API para gerar uma resposta
-        openai.api_key = 'OPENAI_API_KEY'
-        response = openai.Completion.create(
-            engine=model_engine,
-            prompt=openai_prompt + user_message,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.7,
-        )
-
-        # Extrai a resposta gerada pelo OpenAI API
-        response_text = response.choices[0].text
-        response_text = response_text.replace(openai_response_start, "").strip()
-
-        # Envia a resposta para o canal do Discord
-        await message.channel.send(response_text)
+    # Envia a resposta para o canal do Discord
+    await ctx.send(response_text)
 
 
-bot.run('DISCORD_BOT_TOKEN')
+# inicie o bot
+bot.run('MTA3NjY1MDEyMTAxMTQxMzEyMw.GQcpxv.uTM3pJIiGEm5dGX4QpJV1wXr3505alB9tcI1pI')
